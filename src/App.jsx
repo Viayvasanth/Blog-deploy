083e54af0e3f2132639bd02acda8783e749778d8
Vijay <vijayvasanth994@gmail.com>
2026-01-24 07:47:36 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import React from 'react'
-import {BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import {HashRouter  as Router,Route,Routes } from 'react-router-dom'
 
 import './App.css'
 import PostList from './pages/PostList'
